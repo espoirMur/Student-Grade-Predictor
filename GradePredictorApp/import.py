8dd202f51c4c7eda20e7f32aa763eaa030028182
espyMur <espoir.mur@gmail.com>
2017-09-22 11:08:17 +0200

    travis ci try to solve import error v3

--- GradePredictorApp/import.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-cwd = os.getcwd()
-sys.path.insert(0,cwd+"/codes/")
-reload(sys)
-sys.setdefaultencoding('utf8') #for ascii decoder in notes
-print sys.path
--- GradePredictorApp/tests/FrontEndTests.py
+++ GradePredictorApp/tests/FrontEndTests.py
@@ -4,7 +4,6 @@ import json
 from flask import abort, url_for, Flask
 import unittest
 from config import app_config
-
 from app import create_app
 
 
