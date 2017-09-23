96c559660825bb4a9611e0035d1e7893ddf0bbbe
espyMur <espoir.mur@gmail.com>
2017-09-23 09:50:39 +0200

    travis ci by solving import error with module creation V25

--- GradePredictorApp/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='predictives_models_building',
-      version='1.0',
-      description='code for running predictives modeling tasks',
-      author='Espoir Murhabazi',
-      author_email='espoir.mur@gmail.com',
-      url='https://github.com/espoirMur/Memory-Working-Dir',
-      py_modules=['codes/predictiveModelBuilding'],
-     )
