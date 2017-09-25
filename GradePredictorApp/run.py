32b675a50f708a7ada2d34c2de01fa78746efb45
espyMur <espoir.mur@gmail.com>
2017-09-25 19:28:24 +0200

    22th fixing port in use error

--- GradePredictorApp/run.py
+++ GradePredictorApp/run.py
@@ -5,7 +5,7 @@ from app import create_app
 CONFIG_NAME = os.getenv('APP_CONFIG')
 if CONFIG_NAME != 'development':
     app = create_app('production')
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 else :
     app = create_app(CONFIG_NAME)
     if __name__ == '__main__':
