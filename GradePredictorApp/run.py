054c9ed526d70f83938e51ce2387f07f366ff16f
espyMur <espoir.mur@gmail.com>
2017-09-25 19:13:32 +0200

    20th attenpt fixing import error aapp boot error

--- GradePredictorApp/run.py
+++ GradePredictorApp/run.py
@@ -5,8 +5,8 @@ from app import create_app
 CONFIG_NAME = os.getenv('APP_CONFIG')
 app = create_app(CONFIG_NAME)
 if CONFIG_NAME != 'development':
-    if __name__ == '__main__':
-        app.run(debug=True)
+
+    app.run(debug=True, host='0.0.0.0')
 else :
     if __name__ == '__main__':
         app.run(host='localhost', port=9874)
